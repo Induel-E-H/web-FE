09991a03186ff435b57a5f9e276e52265c6a719c
J_Coder <seok3765@naver.com>
2026-03-15 21:33:01 +0900

    ✨ feat: 기업 정보 상수 추가

---
+++ src/shared/constant/company.ts
@@ -0,0 +1,9 @@
+export const COMPANY = {
+  NAME_KO: '인들이앤에이치',
+  NAME_EN: 'INDUEL E&H',
+  NAME_FULL: 'INDUEL Engineering & Holdings',
+  PHONE: '0516266277',
+  PHONE_DISPLAY: '051-626-6277',
+  ADDRESS_MAIN: '부산 남구 수영로 274-16',
+  ADDRESS_SUB: '2호선 경성대부경대역 5번 출구 348m',
+} as const;
