4ad3f77ba5e6a3ac8f3eddf91b25500afdf2ef15
J_Coder <seok3765@naver.com>
2026-03-24 00:24:02 +0900

    ✨ feat: award 페이지 제목 구현

---
+++ src/widgets/award/ui/Award.tsx
@@ -0,0 +1,12 @@
+import '../styles/Award.css';
+import { AwardTitle } from './Title';
+
+function Award() {
+  return (
+    <section className='award'>
+      <AwardTitle></AwardTitle>
+    </section>
+  );
+}
+
+export default Award;
---
+++ src/widgets/award/ui/Title.tsx
@@ -0,0 +1,10 @@
+import '../styles/Title.css';
+
+export function AwardTitle() {
+  return (
+    <div className='award__title'>
+      <h2>Award</h2>
+      <h3>수상 및 인증 내역</h3>
+    </div>
+  );
+}
