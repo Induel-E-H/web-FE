6235a2cc32bf3bcde6202715661e7ab683e02d9a
J_Coder <seok3765@naver.com>
2026-03-27 20:02:49 +0900

    🔨 refactor: 데이터 json -\> ts 변경

--- src/entities/history/index.ts
+++ src/entities/history/index.ts
@@ -1,3 +1,8 @@
-export { default as artworks } from './model/artwork.json';
-export { default as awardData } from './model/award.json';
-export { default as timelineData } from './model/timeline.json';
+export type { ArtworkItem } from './model/artworkData';
+export { ARTWORK_LIST as artworks } from './model/artworkData';
+
+export type { AwardHistoryItem } from './model/awardHistoryData';
+export { AWARD_HISTORY_LIST as awardData } from './model/awardHistoryData';
+
+export type { TimelineItem } from './model/timelineData';
+export { TIMELINE_LIST as timelineData } from './model/timelineData';
