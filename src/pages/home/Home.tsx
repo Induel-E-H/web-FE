5ab09c312baa086b7f9bf3d6e867d47cf3263041
imyourmxxn <pkm5522@naver.com>
2026-04-01 01:07:30 +0900

    🧹 clean: home.tsx 코드 주석처리

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,11 @@
 // import Hero from '@widgets/hero';
-import { Vision } from '@widgets/vision';
+// import { Vision } from '@widgets/vision';
 
 function Home() {
   return (
     <>
       {/* <Hero></Hero> */}
-      <Vision></Vision>
+      {/* <Vision></Vision> */}
     </>
   );
 }
