42906daad7c9556b316b7b29927cc151449d5182
imyourmxxn <pkm5522@naver.com>
2026-04-09 04:39:59 +0900

    ✨ feat: title 디자인 구현

--- src/widgets/patent/ui/Title.tsx
+++ src/widgets/patent/ui/Title.tsx
@@ -3,9 +3,11 @@ import '../styles/Title.css';
 export function PatentTitle() {
   return (
     <div className='patent__title'>
-      <p>PATENTS</p>
-      <h2>특허 취득 기록</h2>
-      <p>끊임없는 기술 개발과 혁신의 증명</p>
+      <p className='patent__title_eng'>PATENTS</p>
+      <h2 className='patent__title_kor'>특허 취득 기록</h2>
+      <p className='patent__title_description'>
+        끊임없는 기술 개발과 혁신의 증명
+      </p>
     </div>
   );
 }
