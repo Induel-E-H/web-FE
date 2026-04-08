13cf0aaa89903aab0cf36359f875e48c43e44449
imyourmxxn <pkm5522@naver.com>
2026-04-09 03:59:29 +0900

    ⚙️ chore: patent data parsing

--- src/entities/patent/index.ts
+++ src/entities/patent/index.ts
@@ -1,2 +1,3 @@
-export { default as PATENT_LIST_DATA } from './model/patentListData';
+export type { PatentValidType, PatentExpireType } from './model/patentListData';
+export { PATENT_VALID_LIST, PATENT_EXPIRE_LIST } from './model/patentListData';
 export { default as PATENT_IMG_DATA } from './model/patentImgData';
