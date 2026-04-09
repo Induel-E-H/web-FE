cd31fb94ccc4b593e78c9865be0c216abee2674e
J_Coder <seok3765@naver.com>
2026-04-09 21:30:47 +0900

    ✨ feat: book cover 구현

--- src/widgets/history/ui/book/Cover.tsx
+++ src/widgets/history/ui/book/Cover.tsx
@@ -1,11 +1,13 @@
+import '../../styles/book/Cover.css';
+
 export function BookCover() {
   return (
     <div className='history__book-cover'>
       <div className='history__book-cover-left'></div>
       <div className='history__book-cover-center'>
         <div className='history__book-cover-center-line'>
-          <div className='history__book-cover-center-line-top'></div>
-          <div className='history__book-cover-center-line-bottom'></div>
+          <div />
+          <div />
         </div>
         <div className='history__book-cover-center-spine'>
           <div className='history__book-cover-center-spine-left'></div>
