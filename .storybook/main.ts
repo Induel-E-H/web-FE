d5ca6351b657d16bee8d30b88484da0ad47b83ec
J_Coder <seok3765@naver.com>
2026-04-16 02:56:57 +0900

    ⚙️ chore: story book path alias lint 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -10,5 +12,6 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
   ],
   framework: '@storybook/react-vite',
+  viteFinal: (config) => mergeConfig(config, { plugins: [tsconfigPaths()] }),
 };
 export default config;
