4c0937ebb4a731336e9a1c2cb6c450c711d98ffa
J_Coder <seok3765@naver.com>
2026-04-22 22:21:09 +0900

    🔨 refactor: google analytics, naver map 동적 로드 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,34 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '@pages/home';
 
 const PrivacyPolicy = lazy(() => import('@pages/privacy-policy'));
 
+const GA_ID = 'G-JMQZ9VKYZ6';
+
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function (...args: unknown[]) {
+      window.dataLayer.push(args);
+    };
+    window.gtag('js', new Date());
+    window.gtag('config', GA_ID);
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
